f8d80d47213e04c9b13814e80911ef70a4785ebb
rivermanvr <vrgithub@riversconsulting.com>
2017-02-22 16:50:48 -0500

    simple server.js listening & lookng for db

--- server.js
+++ server.js
@@ -1,2 +1,9 @@
+const express = require('express');
+const server = express();
 const Sequelize = require('sequelize');
-const acmeDB = new Sequelize('');
+
+const connectDB = process.env.DATABASE_URL || 'postgres://localhost/acme_sql/';
+const acmeDB = new Sequelize(connectDB);
+
+const port = process.env.PORT || 3000;
+server.listen(port, () => console.log(`listening on port ${port}`));
