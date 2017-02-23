3c5f716f71f61f604fbaf1de6a7ff9dddd8763ed
rivermanvr <vrgithub@riversconsulting.com>
2017-02-22 21:52:18 -0500

    created acme_sql setup user & department table and synced

--- server.js
+++ server.js
@@ -2,8 +2,18 @@ const express = require('express');
 const server = express();
 const Sequelize = require('sequelize');
 
-const connectDB = process.env.DATABASE_URL || 'postgres://localhost/acme_sql/';
+const connectDB = process.env.DATABASE_URL || 'postgres://localhost/acme_sql';
 const acmeDB = new Sequelize(connectDB);
+
+const Department = acmeDB.define('department', {
+
+});
+const User = acmeDB.define('user', {
+
+});
+
+acmeDB.sync({ force: true })
+    .catch( err => console.log(err));
 
 const port = process.env.PORT || 3000;
 server.listen(port, () => console.log(`listening on port ${port}`));
