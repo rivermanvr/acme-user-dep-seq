57a8ac5a0b63070dc57e8d70335e2b84ace084a3
rivermanvr <vrgithub@riversconsulting.com>
2017-02-24 00:10:27 -0500

    working on server.js

--- server.js
+++ server.js
@@ -6,14 +6,25 @@ const connectDB = process.env.DATABASE_URL || 'postgres://localhost/acme_sql';
 const acmeDB = new Sequelize(connectDB);
 
 const Department = acmeDB.define('department', {
-
+    name: acmeDB.Sequelize.STRING
 });
 const User = acmeDB.define('user', {
-
+    name: acmeDB.Sequelize.STRING
 });
 
-acmeDB.sync({ force: true })
+const sync = () => {
+    return acmeDB.sync({ force: true });
+};
+
+const seed = () => {
+    return sync()
+        .then();
+};
+
+seed()
+    .then(() => console.log('your data is seeded'))
     .catch( err => console.log(err));
 
+console.log('Models: ', acmeDB.models);
 const port = process.env.PORT || 3000;
 server.listen(port, () => console.log(`listening on port ${port}`));
