86c3713c509852d97c39dbde101077ccc3e996d9
rivermanvr <vrgithub@riversconsulting.com>
2017-02-27 15:48:41 -0500

    setting up test file.

--- test/models.test.js
+++ test/models.test.js
@@ -1,1 +1,5 @@
 const expect = require('chai').expect;
+const Sequelize = require( 'sequelize' );
+const connectDB = process.env.DATABASE_URL || 'postgres://localhost/acme_sql';
+const acmeDB = new Sequelize(connectDB);
+
